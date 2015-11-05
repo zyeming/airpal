ffa904b783a3b3294de909a4cc3a06c1ddcfdb43
Yeming <yeming.zhang@kanchufang.com>
2015-11-05 15:12:12 +0800

    fix a table search bug

--- src/main/java/com/airbnb/airpal/presto/metadata/SchemaCache.java
+++ src/main/java/com/airbnb/airpal/presto/metadata/SchemaCache.java
@@ -56,7 +56,7 @@ public class SchemaCache
                     public Map<String, List<String>> load(String catalogName)
                     {
                         return queryMetadata(format(
-                                "SELECT '%s', table_catalog, table_schema, table_name " +
+                                "SELECT '%s', table_schema, table_name " +
                                         "FROM %s.information_schema.tables ",
                                 catalogName, catalogName));
                     }
